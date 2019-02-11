747eb5dca3125f913c999104083ea28619408647
Jkeslar21 <46494969+Jkeslar21@users.noreply.github.com>
2019-02-11 16:45:42 -0700

    first commit

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent() {
+    return (
+        <div className="CardContent">
+        <h2>Get started with React</h2>
+        <br></br>
+        <p>React makes it painless to create interactive UIs.  Design simple views for each state in your application.</p>
+        <br></br>
+        <a href='www.reactjs.org'>www.reactjs.org</a>
+        </div>
+    )
+}
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return (<div className='HeaderTitle'>
+        <h1 className='title'>Lambda School</h1>
+        <h4 className='timeStamp'>@LambdaSchool · 26 jan</h4>
+    </div>)
+}
+
+export default HeaderTitle;
