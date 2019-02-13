39b75eb6c7cc466e5044f3c1f5122a1986e31f3f
Jkeslar21 <46494969+Jkeslar21@users.noreply.github.com>
2019-02-12 17:06:36 -0700

    touchup

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
-import NumberButton from './components/ButtonComponents/NumberButton';
 import KeyPad from './components/ButtonComponents/Pad'
 
 const App = () => {
